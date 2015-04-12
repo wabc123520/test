2fd1dd5e9ee9363c2a4502414bec424b3d5579ab
zhanghuixiang <wabc123520@163.com>
2015-04-12 14:19:54 +0800

    创建好
    12121

---
+++ 2222/DrawerMenu/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  DrawerMenu
+//
+//  Created by zhanghuixiang on 15/4/12.
+//  Copyright (c) 2015年 Wynton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ 2222/DrawerMenu/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  DrawerMenu
+//
+//  Created by zhanghuixiang on 15/4/12.
+//  Copyright (c) 2015年 Wynton. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"sadsadsa");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
